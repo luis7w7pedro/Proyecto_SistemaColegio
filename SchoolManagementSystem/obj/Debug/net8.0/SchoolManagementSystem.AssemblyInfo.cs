62f23e0aa6b8e0f4f02ee53bfdd482a0c626a063
luis7w7pedro <luispedro98gr@gmail.com>
2025-05-11 15:08:23 -0600

    cambios LP
    actualización LP

--- SchoolManagementSystem/obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
+++ SchoolManagementSystem/obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4accd903f785dbc242484d3b435bb201297faeba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14a96d05ef2226d5964ba76773f7a10cf60c6f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
